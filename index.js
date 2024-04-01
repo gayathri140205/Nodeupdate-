2aad571dabf021f8beb3aad61ef9406311943bae
gayathri140205 <gayathri140205@gmail>
2024-04-01 17:07:46 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
